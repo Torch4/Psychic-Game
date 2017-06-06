11fbda66d6867e9baa4d814fc774ec924be75f71
Adrian Fahrer <acfahrer@gmail.com>
2017-06-05 22:34:25 -0400

    final draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,42 +3,46 @@ var losses = 0
 var guessesleft = 9
 var guessessofar = 0 
 
- 	document.onkeyup = function(event) {
+document.onkeyup = function(event) {
     
 
-    if((event.keyCode >= '97' || event.keyCode <= '122') {
-        var userGuess = event.key;
-        ...
+    if(event.key.match(/^[a-z]$/)) {
+        var userGuess = event.key;    
     }
     else {
         alert("You can only guess a letter!");
     }
-}
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-document.getElementById("guessesfar").innerHTML += userGuess + " ";
+	var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+	document.getElementById("guessesfar").innerHTML += userGuess + " ";
+
+	var computerGuess = letters[Math.floor(Math.random() * letters.length)];
 
 		
 		
       
-       if(userGuess === computerGuess){
+    if(userGuess === computerGuess){
        	wins++; 
        	document.getElementById("wins").innerHTML = "Wins: " + wins;
-       }
-       else (guessesleft--); {
-       	document.getElementById("guessesleft").innerHTML = "guessesleft: " + guessesleft;
-      }
+       	guessesleft = 9;
+      	document.getElementById("guessesleft").innerHTML = "Guesses Left: " + guessesleft;
+      	guessesfar = ""
+      	document.getElementById("guessesfar").innerHTML = "Guesses So Far " + guessesfar;
+    }  	
+    
+    else (guessesleft--); {
+       	document.getElementById("guessesleft").innerHTML = "Guesses Left: " + guessesleft;
+    }
 
-      if(guessesleft < 1) {
+    if(guessesleft < 1) {
       	losses++;
-      	document.getElementById("losses").innerHTML = "losses: " + losses;
-}
-      	if(guessesleft < 1) {
-      	  guessesleft + 9;
-      	  document.getElementById("guessesleft").innerHTML = "guessesleft: " + guessesleft;
-}
-      }       
+      	document.getElementById("losses").innerHTML = "Losses: " + losses;
+	    guessesleft = 9;
+      	document.getElementById("guessesleft").innerHTML = "Guesses Left: " + guessesleft;
+      	guessesfar = ""
+      	document.getElementById("guessesfar").innerHTML = "Guesses So Far: " + guessesfar;
+	}
+            
    
-     }
+}
